9629bd9e4d4a4d6fcdf48b320f5fcd19eb85824c
Erik-Russell <erik.z.russell@gmail.com>
2024-11-11 11:35:49 -0800

    Posts Index View with hidden Author
    Author only viewable if user logged in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   before_action :require_login, only: [:new, :create]
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
   private
 
   def require_login
