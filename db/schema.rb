18f98101118235f2605e0e9770bbef5c5bfd9bdd
Erik-Russell <erik.z.russell@gmail.com>
2024-11-11 10:59:40 -0800

    Add User Reference to Posts Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_11_175543) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_11_185723) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_11_175543) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
