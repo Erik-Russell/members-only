1a45ee70e9acbd3b20a8baa6fdfb957e78e8707a
Erik-Russell <erik.z.russell@gmail.com>
2024-11-11 10:15:32 -0800

    User Login Requirement
    login required to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,13 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
+  private
+
+  def require_login
+    if user_signed_in?
+      # allow user to perform actions
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
